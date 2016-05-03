aa7fe75eab3f83d1be8ed1bc2b2b92361d549f64
Pepijn Senders <ps@hellofresh.com>
2016-05-03 18:43:15 +0200

    Clean test.js

--- index.js
+++ index.js
@@ -3,33 +3,4 @@ require('promise.prototype.finally');
 
 const Lentil = require('./lib/Lentil');
 
-const lentil = new Lentil({
-    paths: {
-        modules: __dirname + '/__test__/modules',
-        libs: __dirname + '/__test__/libs',
-        dist: __dirname + '/__test__/dist',
-        tmp: __dirname + '/__test__/tmp',
-        rootPrefix: '/'
-    },
-    libs: {
-        'test': [
-            __dirname + '/__test__/libs/test.js'
-        ]
-    },
-    karma: {
-        configFile: __dirname + '/karma.conf.js'
-    },
-    plugins: {
-        Uglify: {
-            test: true
-        },
-        eslint: {
-            baseConfig: {
-                extends: 'eslint:recommended'
-            },
-            globals: ['lentil', 'angular', 'window', 'document', 'CustomEvent']
-        }
-    }
-});
-
 module.exports = Lentil;
---
+++ test.js
@@ -0,0 +1,47 @@
+const Lentil = require('.');
+
+const lentil = new Lentil({
+    paths: {
+        modules: __dirname + '/__test__/modules',
+        libs: __dirname + '/__test__/libs',
+        dist: __dirname + '/__test__/dist',
+        tmp: __dirname + '/__test__/tmp',
+        rootPrefix: '/'
+    },
+    libs: {
+        'test': [
+            __dirname + '/__test__/libs/test.js'
+        ]
+    },
+    karma: {
+        configFile: __dirname + '/karma.conf.js'
+    },
+    plugins: {
+        eslint: {
+            baseConfig: {
+                extends: 'eslint:recommended'
+            },
+            globals: ['lentil', 'angular', 'window', 'document', 'CustomEvent']
+        }
+    }
+});
+
+lentil.runModuleTask('test', 'angular');
+lentil.runModuleTask('test', 'angular', {
+    shouldMinify: true
+});
+
+lentil.runModuleTask('test', 'js');
+lentil.runModuleTask('test', 'js', {
+    shouldMinify: true
+});
+
+lentil.runModuleTask('test', 'sass');
+lentil.runModuleTask('test', 'sass', {
+    shouldMinify: true
+});
+
+lentil.runModuleTask('', 'libs');
+lentil.runModuleTask('', 'libs', {
+    shouldMinify: true
+});

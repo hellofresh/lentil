321816dd956ef849271bede59c385015ae20340d
Pepijn Senders <ps@hellofresh.com>
2016-05-09 15:33:23 +0200

    Usability and speed fixes

--- bin/lentil.js
+++ bin/lentil.js
@@ -2,62 +2,77 @@
 require('sugar');
 
 const cluster = require('cluster');
+const fs = require('fs');
+const path = require('path');
 const numCpus = require('os').cpus().length;
 const Logger = require('../lib/Logger');
-
+const FileSystem = require('../lib/FileSystem');
+const Command = require('../lib/Command');
+const CommandsRunner = require('../lib/CommandsRunner');
 
 const argv = require('yargs').argv;
 const args = argv._;
 
-if (Number.isInteger(argv.logLevel)) {
-    Logger.setLogLevel(argv.logLevel);
-} else if (Number.isInteger(process.env.LOG_LEVEL)) {
-    Logger.setLogLevel(process.env.LOG_LEVEL);
-} else {
-    Logger.setLogLevel(2);
-}
-
-const handleException = (message) => {
-    Logger.error(new Error(message));
-    process.exit(1);
-};
-
 if (!argv.configFile) {
-    handleException('No configFile specified.');
+    throw new Error('No configFile specified.');
 }
 
-const taskNameArg = args[1];
+const CWD = process.cwd();
+const config = require(`${CWD}/${argv.configFile}`); // eslint-disable-line global-require
+
+Logger.setLogLevel(argv.logLevel);
+
+let modules;
+if (!args[0]) {
+    try {
+        var modulesPath = config.paths.modules;
+    } catch (e) {
+        throw new Error('Path to modules not defined in configuration.');
+    }
+
+    const availableModules = fs.readdirSync(modulesPath).filter(function(file) {
+        return fs.statSync(path.join(modulesPath, file)).isDirectory();
+    });
+
+    modules = [availableModules];
+} else {
+    modules = [args[0]];
+}
+
+let tasks;
+if (!args[1]) {
+    tasks = ['angular', 'js', 'sass'];
+} else {
+    tasks = [args[1]];
+}
 
 if (cluster.isMaster) {
     Logger.info(`Welcome to lentil, you have ${numCpus} workers available.`);
 
-    let tasks;
-    if (!taskNameArg) {
-        tasks = ['angular', 'js', 'sass'];
-    } else {
-        tasks = [taskNameArg];
-    }
+    const commandsRunner = new CommandsRunner(modules, tasks);
 
-    const CWD = process.cwd();
-    const config = require(`${CWD}/${argv.configFile}`); // eslint-disable-line global-require
+    const commands = commandsRunner.seedCommands(numCpus, argv, config);
 
     Promise.all(
-        tasks.map((taskName) => new Promise((resolve, reject) => {
-            const worker = cluster.fork({
-                RUN_TASK: taskName,
-                config: JSON.stringify(config),
-            });
+        commands.filter(
+            (commandCollection) => commandCollection.length
+        ).map(
+            (commandCollection) => new Promise((resolve, reject) => {
+                const worker = cluster.fork({
+                    commands: JSON.stringify(commandCollection),
+                });
 
-            worker.on('message', (data) => {
-                worker.kill('SIGTERM');
+                worker.on('message', (data) => {
+                    worker.kill('SIGTERM');
 
-                if (data.event === 'finished') {
-                    resolve(data);
-                } else {
-                    reject(data);
-                }
-            });
-        }))
+                    if (data.event === 'finished') {
+                        resolve(data);
+                    } else {
+                        reject(data);
+                    }
+                });
+            })
+        )
     ).then((result) => {
         const hasError = result.length && result.sum(
             (worker) => worker.errorCount || 0
@@ -74,37 +89,11 @@ if (cluster.isMaster) {
 } else {
     Logger.info('Starting worker.');
 
-    const Lentil = require('..'); // eslint-disable-line global-require
+    const rawCommands = JSON.parse(process.env.commands);
 
-    const lentil = new Lentil(JSON.parse(process.env.config));
+    for (let rawCommand of rawCommands) {
+        const command = Command.ofRaw(rawCommand);
 
-    const moduleName = args[0];
-
-    if (!argv.watch) {
-        lentil.run(moduleName, process.env.RUN_TASK, {
-            shouldMinify: argv.minify,
-        }).then((result = {}) => {
-            process.nextTick(() => {
-                Logger.info(`Worker ID ${process.pid} finished.`);
-
-                process.send({
-                    event: 'finished',
-                    errorCount: result.errorCount,
-                });
-            });
-        }).catch(() => {
-            process.nextTick(() => {
-                Logger.info(`Worker ID ${process.pid} finished with errors.`);
-
-                process.send({
-                    event: 'finished',
-                    errorCount: 1,
-                });
-            });
-        });
-    } else {
-        lentil.watch(moduleName, process.env.RUN_TASK, {
-            shouldMinify: argv.minify,
-        });
+        command.run();
     }
 }
--- example/dist/test-angular.js
+++ example/dist/test-angular.js
@@ -1,6 +1,19 @@
+var angular = {
+    module: function() {
+        return {
+            controller: function() {}
+        }
+    }
+};
+
+var app = angular.module('test', []);
+
+app.controller('TestCtrl', ['$location', function($location) {
+}]);
 
 (function(angular) {
     angular.module('test')
         .run(['$templateCache', function($templateCache) {
+            $templateCache.put('/test.html', '<p class=test ng-class="{ test: \'test\' }">test</p><h3>test</h3>');
         }]);
 }(angular));

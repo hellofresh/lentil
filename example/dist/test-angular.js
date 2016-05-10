00c10a6d15ca693118c70abe2c1024cb060fd3d1
Pepijn Senders <ps@hellofresh.com>
2016-05-10 12:57:31 +0200

    mkdirp

--- example/dist/test-angular.js
+++ example/dist/test-angular.js
@@ -12,7 +12,7 @@ app.controller('TestCtrl', ['$location', function($location) {
 }]);
 
 (function(angular) {
-    angular.module('test')
+    angular.module('lentil.test')
         .run(['$templateCache', function($templateCache) {
             $templateCache.put('/test.html', '<p class=test ng-class="{ test: \'test\' }">test</p><h3>test</h3>');
         }]);
